37622c33db25f141b35de910a693717764412f13
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-07-18 23:58:53 +0530

    update webapp as well as website

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
+'use client'
 import PriceList from "./components/PriceList";
-import Features from "./components/Features";
 import Bulk from "./components/Bulk";
 import QualitySection from "./components/QualitySection";
 import BulkBenefitsSection from "./components/BulkBenefitsSection";
@@ -8,8 +8,20 @@ import Footer from "./components/Footer";
 import Stat from "./components/Stat";
 import SustainabilitySection from "./components/SustainabilitySection";
 import TestimonialCarousel from "./components/TestimonialCarousel";
+import { useRouter } from 'next/navigation';
+import { useAuth } from '../app/context/AuthContext';
+import React, { useEffect } from 'react';
 
 export default function Home() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.replace('/webapp');
+    }
+  }, [isAuthenticated]);
+
   return (
     <main className="font-sans bg-white text-black">
       {/* <Sample/> */}
